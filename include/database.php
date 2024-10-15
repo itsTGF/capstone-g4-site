45399a585b79a7b6a07ade427242b6181fe82871
Benny Pena <bennypena1337@gmail.com>
2024-10-15 11:18:42 -0400

    initial commit

---
+++ include/database.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Gets the database connection
+ * @return object Connection to the database
+ */
+
+ function getDB() {
+    $serverName = "tcp:" . getenv('DB_SERVER_NAME') . ",1433";
+    $connectionOptions = array(
+        "Database" => getenv('DB_NAME'),
+        "Uid" => getenv('DB_UID'),
+        "PWD" => getenv('DB_PWD')
+    );
+
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    if ($conn) {
+        echo("Connected to the database!" . PHP_EOL);
+        return $conn;
+    } else {
+        die(print_r(sqlsrv_errors(), true));
+    }
+ }
+ ?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require 'include/database.php';
+
+echo("Hello World!" . PHP_EOL);
+
+$conn = getDB();
+?>
