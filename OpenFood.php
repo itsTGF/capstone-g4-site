0ee612b34bf7776823e1b3ed9d6c03749a4a2da9
JtG902 <155279571+JtG902@users.noreply.github.com>
2024-11-11 11:03:46 -0500

    Update and rename PageStep.php to OpenFood.php
    Changed layout.

---
+++ OpenFood.php
@@ -0,0 +1,86 @@
+<?php
+require 'include/database.php';
+
+$conn = getDB();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Open Food Facts</title>
+	
+	<link rel="stylesheet" href="styles.css">
+    
+</head>
+<body>
+    <header id="topPage">
+        <h1>Open Food Facts</h1>
+    </header>
+    
+    <h3>Items:</h3>
+    
+    <!-- database access -->
+    
+    <?php
+    $sql = "SELECT * FROM goglia_items WHERE ID='3017620422003'"; //take out WHERE part
+    
+    $stmt = sqlsrv_query($conn, $sql);
+    
+    if ($stmt === false) {
+        die(print_r(sqlsrv_errors(), true));
+    }
+    
+    echo "<table border='1'>
+        <tr>
+            <th>ID</th>
+            <th>Product</th>
+            <th>Generic Name</th>
+            <th>Brand</th>
+            <th>Food Groups</th>
+            <th>Quantity</th>
+            <th>Serving Size</th>
+            <th>Origin</th>
+            <th>Ingredients</th>
+            <th>Allergens</th>
+            <th>Nutri-Score</th>
+            <th>Stores Sold In</th>
+        </tr>";
+   
+    while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+        echo "<tr>
+            <td>" . $row['id'] . "</td>
+            <td>" . $row['prodname'] . "</td>
+            <td>" . $row['genname'] . "</td>
+            <td>" . $row['brand'] . "</td>
+            <td>" . $row['foodgroups'] . "</td>
+            <td>" . $row['quantity'] . "</td>
+            <td>" . $row['serving'] . "</td>
+            <td>" . $row['origin'] . "</td>
+            <td>" . $row['ingredients'] . "</td>
+            <td>" . $row['allergens'] . "</td>
+            <td>" . $row['nutriscore'] . "</td>
+            <td>" . $row['stores'] . "</td>
+        </tr>";
+    }
+    
+    echo "</table>";
+    sqlsrv_free_stmt($stmt);
+    ?>
+    
+    <!-- end database access -->
+    
+    <h3>API:</h3>
+    
+    <nav>
+        <a href="https://world.openfoodfacts.org/">Open Food Facts Homepage</a>
+    </nav>
+
+    <footer>
+        <p>Return to the top of the <a href="#topPage">page</a>.</p>
+    </footer>
+</body>
+</html>
